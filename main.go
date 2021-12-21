17382f63a0e8639d61a24ebadcbf0eb1233352d6
Arnob kumar saha <arnobkumarsaha00@gmail.com>
2021-12-21 10:39:01 +0600

    upto 1.7 -> types.go,  writting controller, main.go

--- main.go
+++ main.go
@@ -80,6 +80,7 @@ func main() {
 
 	if err = (&controllers.NiceJobReconciler{
 		Client: mgr.GetClient(),
+		Log:    ctrl.Log.WithName("controllers").WithName("NiceJob"),
 		Scheme: mgr.GetScheme(),
 	}).SetupWithManager(mgr); err != nil {
 		setupLog.Error(err, "unable to create controller", "controller", "NiceJob")
